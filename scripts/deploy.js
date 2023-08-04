3dad72f748b69076ad527c4e4923493855e0a1f5
ThomasHeim11 <wearedreamin@gmail.com>
2023-08-04 15:42:10 +0200

    update deploy script✅

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,9 +1,4 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
+
 const hre = require("hardhat")
 const { items } = require("../src/items.json")
 
@@ -12,11 +7,35 @@ const tokens = (n) => {
 }
 
 async function main() {
+  // Setup accounts
+  const [deployer] = await ethers.getSigners()
 
+  // Deploy Blockshop
+  const Blockshop = await hre.ethers.getContractFactory("Blockshop")
+  const blockshop = await Blockshop.deploy()
+  await blockshop.deployed()
+
+  console.log(`Deployed Blockshop Contract at: ${blockshop.address}\n`)
+
+  // Listing items...
+  for (let i = 0; i < items.length; i++) {
+    const transaction = await blockshop.connect(deployer).list(
+      items[i].id,
+      items[i].name,
+      items[i].category,
+      items[i].image,
+      tokens(items[i].price),
+      items[i].rating,
+      items[i].stock,
+    )
+
+    await transaction.wait()
+
+    console.log(`Listed item ${items[i].id}: ${items[i].name}`)
+  }
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
