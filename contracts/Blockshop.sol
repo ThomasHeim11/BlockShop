d041a96a62934779436aa281c3386de0d26884ee
ThomasHeim11 <wearedreamin@gmail.com>
2023-08-04 14:59:43 +0200

    update smart contract✅

--- contracts/Blockshop.sol
+++ contracts/Blockshop.sol
@@ -20,8 +20,8 @@ contract Blockshop {
     }
 
     mapping(uint256 => Item) public items;
-    mapping(address => uint256) public orderCount;
     mapping(address => mapping(uint256 => Order)) public orders;
+    mapping(address => uint256) public orderCount;
 
     event Buy(address buyer, uint256 orderId, uint256 itemId);
     event List(string name, uint256 cost, uint256 quantity);
@@ -44,31 +44,50 @@ contract Blockshop {
         uint256 _rating,
         uint256 _stock
     ) public onlyOwner {
-        Item memory item = Item(_id, _name, _category, _image, _cost, _rating, _stock);
+        // Create Item
+        Item memory item = Item(
+            _id,
+            _name,
+            _category,
+            _image,
+            _cost,
+            _rating,
+            _stock
+        );
 
+        // Add Item to mapping
         items[_id] = item;
 
+        // Emit event
         emit List(_name, _cost, _stock);
     }
 
     function buy(uint256 _id) public payable {
+        // Fetch item
         Item memory item = items[_id];
 
+        // Require enough ether to buy item
         require(msg.value >= item.cost);
+
+        // Require item is in stock
         require(item.stock > 0);
 
+        // Create order
         Order memory order = Order(block.timestamp, item);
-        orderCount[msg.sender]++;
 
-        orderCount[msg.sender]++;
+        // Add order for user
+        orderCount[msg.sender]++; // <-- Order ID
         orders[msg.sender][orderCount[msg.sender]] = order;
+
+        // Subtract stock
         items[_id].stock = item.stock - 1;
 
+        // Emit event
         emit Buy(msg.sender, orderCount[msg.sender], item.id);
     }
 
     function withdraw() public onlyOwner {
-        (bool success,) = owner.call{value: address(this).balance}("");
+        (bool success, ) = owner.call{value: address(this).balance}("");
         require(success);
     }
 }
