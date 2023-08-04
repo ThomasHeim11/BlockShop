da5dfa2766b648c8f21dd4ac79157b1823bc9650
ThomasHeim11 <wearedreamin@gmail.com>
2023-08-04 12:25:27 +0200

    update contract✅

--- contracts/Blockshop.sol
+++ contracts/Blockshop.sol
@@ -4,18 +4,42 @@ pragma solidity ^0.8.9;
 contract Blockshop {
     address public owner;
 
+    struct Item {
+        uint256 id;
+        string name;
+        string category;
+        string image;
+        uint256 cost;
+        uint256 rating;
+        uint256 stock;
+    }
+
+    mapping(uint256 => Item) public items;
+
     constructor() {
         owner = msg.sender;
     }
 
     function list(
-        uint256 _id, 
-        string memory _name, 
+        uint256 _id,
+        string memory _name,
         string memory _category,
+        string memory _image,
         uint256 _cost,
         uint256 _rating,
         uint256 _stock
-        ) public {
+    ) public {
 
+        Item memory item = Item(
+            _id,
+            _name,
+            _category,
+            _image, 
+            _cost, 
+            _rating, 
+            _stock
+        );
+
+        items[_id] = item;
     }
 }
