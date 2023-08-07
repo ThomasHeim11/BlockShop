dab885de28c945253338f45c91d102f359b01c02
ThomasHeim11 <wearedreamin@gmail.com>
2023-08-07 10:03:54 +0200

    update ui✅

--- src/App.js
+++ src/App.js
@@ -14,14 +14,22 @@ import config from "./config.json";
 import { use } from "chai";
 
 function App() {
+  const [provider, setProvider] = useState(null)
   const [account, setAccount] = useState(null);
 
   const loadBlockchainData = async () => {
-    const accounts = await window.ethereum.request({
-      method: "eth_requestAccounts",
-    });
-    const account = ethers.utils.getAddress(accounts[0]);
-    setAccount(account);
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    setProvider(provider)
+
+    const network = await provider.getNetwork()
+    console.log(network)
+
+    const blockshop = new ethers.Contract(
+      "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+      Blockshop,
+      provider
+    )
+  
   };
 
   useEffect(() => {
