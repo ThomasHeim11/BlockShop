6f96ec8c53edb907b9eb6b728d849b507cdd5721
ThomasHeim11 <wearedreamin@gmail.com>
2023-08-07 13:49:48 +0200

    update deploy script✅

--- src/App.js
+++ src/App.js
@@ -1,78 +1,80 @@
-import { useEffect, useState } from "react";
-import { ethers } from "ethers";
+import { useEffect, useState } from 'react'
+import { ethers } from 'ethers'
 
 // Components
-import Navigation from "./components/Navigation";
-import Section from "./components/Section";
-import Product from "./components/Product";
+import Navigation from './components/Navigation'
+import Section from './components/Section'
+import Product from './components/Product'
 
 // ABIs
-import Blockshop from "./abis/Blockshop.json";
+import Blockshop from './abis/Blockshop.json'
 
 // Config
-import config from "./config.json";
-import { use } from "chai";
+import config from './config.json'
 
 function App() {
-  const [provider, setProvider] = useState(null);
-  const [blockshop, setBlockshop] = useState(null);
+  const [provider, setProvider] = useState(null)
+  const [blockshop, setBlockshop] = useState(null)
 
-  const [account, setAccount] = useState(null);
+  const [account, setAccount] = useState(null)
 
-  const [electronics, setElectronics] = useState(null);
-  const [clothing, setClothing] = useState(null);
-  const [toys, setToys] = useState(null);
+  const [electronics, setElectronics] = useState(null)
+  const [clothing, setClothing] = useState(null)
+  const [toys, setToys] = useState(null)
 
-  const togglePop = () => {};
+  const [item, setItem] = useState({})
+  const [toggle, setToggle] = useState(false)
+
+  const togglePop = (item) => {
+    setItem(item)
+    toggle ? setToggle(false) : setToggle(true)
+  }
 
   const loadBlockchainData = async () => {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    setProvider(provider);
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    setProvider(provider)
+    const network = await provider.getNetwork()
 
-    const network = await provider.getNetwork();
-    console.log(network);
+    const blockshop = new ethers.Contract(config[network.chainId].blockshop.address, blockshop, provider)
+    setBlockshop(blockshop)
 
-    const blockshop = new ethers.Contract(
-      config[network.chainId].blockshop.address,
-      Blockshop,
-      provider
-    );
-    setBlockshop(blockshop);
-
-    const items = [];
+    const items = []
 
     for (var i = 0; i < 9; i++) {
-      const item = await blockshop.items(i + 1);
-      items.push(item);
+      const item = await blockshop.items(i + 1)
+      items.push(item)
     }
 
-    const electronics = items.filter((item) => item.category === "electronics");
-    const clothing = items.filter((item) => item.category === "Clothing");
-    const toys = items.filter((item) => item.category === "toys");
+    const electronics = items.filter((item) => item.category === 'electronics')
+    const clothing = items.filter((item) => item.category === 'clothing')
+    const toys = items.filter((item) => item.category === 'toys')
 
-    setElectronics(electronics);
-    setClothing(clothing);
-    setToys(toys);
-  };
+    setElectronics(electronics)
+    setClothing(clothing)
+    setToys(toys)
+  }
 
   useEffect(() => {
-    loadBlockchainData();
-  }, []);
+    loadBlockchainData()
+  }, [])
 
   return (
     <div>
       <Navigation account={account} setAccount={setAccount} />
-      <h2>BlockShop Best Sellers</h2>
 
-      {electronics &&
-        clothing &&
-        toys(
-          <Section
-            title={"Clothing & Jewelry"}
-            items={clothing}
-            togglePop={togglePop}
-          />
-        )}
+      <h2>Blockshop Best Sellers</h2>
+
+      {electronics && clothing && toys && (
+        <>
+          <Section title={"Clothing & Jewelry"} items={clothing} togglePop={togglePop} />
+          <Section title={"Electronics & Gadgets"} items={electronics} togglePop={togglePop} />
+          <Section title={"Toys & Gaming"} items={toys} togglePop={togglePop} />
+        </>
+      )}
+
+      {toggle && (
+        <Product item={item} provider={provider} account={account} blockshop={blockshop} togglePop={togglePop} />
+      )}
     </div>
   );
 }
